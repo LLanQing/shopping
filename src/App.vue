8d7380cd9e987ed55d4adde40f7bbc83c69f6c2a
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-06 22:54:04 +0800

    入口文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<Header></Header>
+		<!-- 路由组件出口的地方:展示的地方 -->
+		<router-view></router-view>
+		中间待补充
+		<Footer v-show="$route.meta.isFooterShow"></Footer>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Header,
+		Footer,
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+// 引入vue-router
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+// 引入router,路径不写全，默认引入文件夹下的index文件
+import router from "./router";
+
+//关闭生产提示
+Vue.config.productionTip = false;
+//使用vue-router插件
+Vue.use(VueRouter);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	// 注册App组件
+	render: h => h(App),
+	// 注册路由，router配置简写方式
+	router,
+}).$mount("#app"); //指定根容器
