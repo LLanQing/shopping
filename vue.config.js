7934386fcd016af7bcd697da4f1281f6436f920e
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-15 11:27:14 +0800

    配置代理服务器解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	// 关闭eslint代码保存校验
 	lintOnSave: false,
+	//开启代理服务器解决跨域问题
+	devServer: {
+		proxy: {
+			// 当请求的前缀是/api，直接转发请求到服务器5500端口
+			'/api': {
+				target: 'http://gmall-h5-api.atguigu.cn',
+			},
+		},
+	},
 });
