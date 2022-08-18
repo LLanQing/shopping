88fdd03e8b601aa3b4bad07e6b8bd693feb9c4ed
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-08-18 21:50:25 +0800

    商品三级分类菜单

--- src/components/TypeNav.vue
+++ src/components/TypeNav.vue
@@ -2,7 +2,59 @@
 	<!-- 商品分类导航,三级菜单 -->
 	<div class="type-nav">
 		<div class="container">
-			<h2 class="all">全部商品分类</h2>
+			<!-- 绑定鼠标移入移出事件来控制商品分类显示 -->
+			<div @mouseenter="showSort" @mouseleave="hideSort">
+				<h2 class="all">全部商品分类</h2>
+				<!-- 添加过渡动画 -->
+				<transition name="sort">
+					<div class="sort" v-show="isShow">
+						<!--利用事件的委派，给所有分类的父节点绑定点击事件，这样就不用给每个a标签绑定事件 -->
+						<!--事件委派问题：
+						（1）如何确定我们点击的一定是a标签呢？如何保证我们只能通过点击a标签才跳转呢？
+						（2）如何获取子节点标签的商品名称和商品id(**我们是通过商品名称和商品id进行页面跳转的**) 
+						-->
+						<div class="all-sort-list2" @click="goSearch">
+							<div class="item" v-for="c1 in categoryList" :key="c1.categoryId">
+								<h3>
+									<!-- 解决问题(1)：为三个等级的a标签添加data-*自定义属性data-categoryName绑定商品标签名称来标识a标签 -->
+									<!-- 解决问题(2)：为三个等级的a标签再添加data-*自定义属性data-categoryId来获取三个等级a标签的商品id，用于路由跳转。 -->
+									<a
+										:data-categoryName="c1.categoryName"
+										:data-category1Id="c1.categoryId"
+										>{{ c1.categoryName }}</a
+									>
+								</h3>
+								<div class="item-list clearfix">
+									<div
+										class="subitem"
+										v-for="c2 in c1.categoryChild"
+										:key="c2.categoryId"
+									>
+										<dl class="fore">
+											<dt>
+												<a
+													:data-categoryName="c2.categoryName"
+													:data-category2Id="c2.categoryId"
+													>{{ c2.categoryName }}</a
+												>
+											</dt>
+											<dd>
+												<em v-for="c3 in c2.categoryChild" :key="c3.categoryId">
+													<a
+														:data-categoryName="c3.categoryName"
+														:data-category3Id="c3.categoryId"
+														>{{ c3.categoryName }}</a
+													>
+												</em>
+											</dd>
+										</dl>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</transition>
+			</div>
 			<nav class="nav">
 				<a href="###">服装城</a>
 				<a href="###">美妆馆</a>
@@ -13,526 +65,223 @@
 				<a href="###">有趣</a>
 				<a href="###">秒杀</a>
 			</nav>
-			<div class="sort">
-				<div class="all-sort-list2">
-					<div class="item bo">
-						<h3>
-							<a href="">图书、音像、数字商品</a>
-						</h3>
-						<div class="item-list clearfix">
-							<div class="subitem">
-								<dl class="fore">
-									<dt>
-										<a href="">电子书</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">婚恋/两性</a>
-										</em>
-										<em>
-											<a href="">文学</a>
-										</em>
-										<em>
-											<a href="">经管</a>
-										</em>
-										<em>
-											<a href="">畅读VIP</a>
-										</em>
-									</dd>
-								</dl>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<h3>
-							<a href="">家用电器</a>
-						</h3>
-						<div class="item-list clearfix">
-							<div class="subitem">
-								<dl class="fore">
-									<dt>
-										<a href="">电子书1</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">免费</a>
-										</em>
-										<em>
-											<a href="">小说</a>
-										</em>
-										<em>
-											<a href="">励志与成功</a>
-										</em>
-										<em>
-											<a href="">婚恋/两性</a>
-										</em>
-										<em>
-											<a href="">文学</a>
-										</em>
-										<em>
-											<a href="">经管</a>
-										</em>
-										<em>
-											<a href="">畅读VIP</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>
-										<a href="">数字音乐</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">通俗流行</a>
-										</em>
-										<em>
-											<a href="">古典音乐</a>
-										</em>
-										<em>
-											<a href="">摇滚说唱</a>
-										</em>
-										<em>
-											<a href="">爵士蓝调</a>
-										</em>
-										<em>
-											<a href="">乡村民谣</a>
-										</em>
-										<em>
-											<a href="">有声读物</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>
-										<a href="">音像</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">音乐</a>
-										</em>
-										<em>
-											<a href="">影视</a>
-										</em>
-										<em>
-											<a href="">教育音像</a>
-										</em>
-										<em>
-											<a href="">游戏</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>文艺</dt>
-									<dd>
-										<em>
-											<a href="">小说</a>
-										</em>
-										<em>
-											<a href="">文学</a>
-										</em>
-										<em>
-											<a href="">青春文学</a>
-										</em>
-										<em>
-											<a href="">传记</a>
-										</em>
-										<em>
-											<a href="">艺术</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>人文社科</dt>
-									<dd>
-										<em>
-											<a href="">历史</a>
-										</em>
-										<em>
-											<a href="">心理学</a>
-										</em>
-										<em>
-											<a href="">政治/军事</a>
-										</em>
-										<em>
-											<a href="">国学/古籍</a>
-										</em>
-										<em>
-											<a href="">哲学/宗教</a>
-										</em>
-										<em>
-											<a href="">社会科学</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>经管励志</dt>
-									<dd>
-										<em>
-											<a href="">经济</a>
-										</em>
-										<em>
-											<a href="">金融与投资</a>
-										</em>
-										<em>
-											<a href="">管理</a>
-										</em>
-										<em>
-											<a href="">励志与成功</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>生活</dt>
-									<dd>
-										<em>
-											<a href="">家庭与育儿</a>
-										</em>
-										<em>
-											<a href="">旅游/地图</a>
-										</em>
-										<em>
-											<a href="">烹饪/美食</a>
-										</em>
-										<em>
-											<a href="">时尚/美妆</a>
-										</em>
-										<em>
-											<a href="">家居</a>
-										</em>
-										<em>
-											<a href="">婚恋与两性</a>
-										</em>
-										<em>
-											<a href="">娱乐/休闲</a>
-										</em>
-										<em>
-											<a href="">健身与保健</a>
-										</em>
-										<em>
-											<a href="">动漫/幽默</a>
-										</em>
-										<em>
-											<a href="">体育/运动</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>科技</dt>
-									<dd>
-										<em>
-											<a href="">科普</a>
-										</em>
-										<em>
-											<a href="">IT</a>
-										</em>
-										<em>
-											<a href="">建筑</a>
-										</em>
-										<em>
-											<a href="">医学</a>
-										</em>
-										<em>
-											<a href="">工业技术</a>
-										</em>
-										<em>
-											<a href="">电子/通信</a>
-										</em>
-										<em>
-											<a href="">农林</a>
-										</em>
-										<em>
-											<a href="">科学与自然</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>少儿</dt>
-									<dd>
-										<em>
-											<a href="">少儿</a>
-										</em>
-										<em>
-											<a href="">0-2岁</a>
-										</em>
-										<em>
-											<a href="">3-6岁</a>
-										</em>
-										<em>
-											<a href="">7-10岁</a>
-										</em>
-										<em>
-											<a href="">11-14岁</a>
-										</em>
-									</dd>
-								</dl>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<h3>
-							<a href="">手机、数码、充值</a>
-						</h3>
-						<div class="item-list clearfix">
-							<div class="subitem">
-								<dl class="fore">
-									<dt>
-										<a href="">充值</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="recharge.html">充值</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>
-										<a href="">电子书2</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">免费</a>
-										</em>
-										<em>
-											<a href="">小说</a>
-										</em>
-										<em>
-											<a href="">励志与成功</a>
-										</em>
-										<em>
-											<a href="">婚恋/两性</a>
-										</em>
-										<em>
-											<a href="">文学</a>
-										</em>
-										<em>
-											<a href="">经管</a>
-										</em>
-										<em>
-											<a href="">畅读VIP</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>
-										<a href="">数字音乐</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">通俗流行</a>
-										</em>
-										<em>
-											<a href="">古典音乐</a>
-										</em>
-										<em>
-											<a href="">摇滚说唱</a>
-										</em>
-										<em>
-											<a href="">爵士蓝调</a>
-										</em>
-										<em>
-											<a href="">乡村民谣</a>
-										</em>
-										<em>
-											<a href="">有声读物</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>
-										<a href="">音像</a>
-									</dt>
-									<dd>
-										<em>
-											<a href="">音乐</a>
-										</em>
-										<em>
-											<a href="">影视</a>
-										</em>
-										<em>
-											<a href="">教育音像</a>
-										</em>
-										<em>
-											<a href="">游戏</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>文艺</dt>
-									<dd>
-										<em>
-											<a href="">小说</a>
-										</em>
-										<em>
-											<a href="">文学</a>
-										</em>
-										<em>
-											<a href="">青春文学</a>
-										</em>
-										<em>
-											<a href="">传记</a>
-										</em>
-										<em>
-											<a href="">艺术</a>
-										</em>
-									</dd>
-								</dl>
-								<dl class="fore">
-									<dt>人文社科</dt>
-									<dd>
-										<em>
-											<a href="">历史</a>
-										</em>
-										<em>
-											<a href="">心理学</a>
-										</em>
-										<em>
-											<a href="">政治/军事</a>
-										</em>
-										<em>
-											<a href="">国学/古籍</a>
-										</em>
-										<em>
-											<a href="">哲学/宗教</a>
-										</em>
-										<em>
-											<a href="">社会科学</a>
-										</em>
-									</dd>
-								</dl>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {
-	name: "TypeNav",
-};
+	// 引入mapState、mapGetters、mapActions、mapMutations
+	import { mapState } from 'vuex';
+
+	export default {
+		name: 'TypeNav',
+		data() {
+			return { isShow: false };
+		},
+		computed: {
+			//自己写计算属性
+			/* categoryList() {
+				return this.$store.state.home.categoryList;
+			}, */
+			//借助mapState生成计算属性：categoryList（对象写法）,属性要写成一个函数
+			// ...mapState('home', { categoryList: state => state.categoryList }),
+			//数组写法
+			...mapState('home', ['categoryList']),
+		},
+		mounted() {
+			// console.log(this.$route);
+			//如果是Home组件，就显示TypeNav
+			if (this.$route.path === '/home') this.isShow = true;
+		},
+		methods: {
+			//三级分类路由跳转到搜索页面的事件回调
+			goSearch(event) {
+				// 函数中传入event参数，获取当前的点击事件，通过event.target属性获取当前点击节点，
+				// 再通过dataset属性获取节点的自定义属性信息(会自动去除data-前缀，且大写变为小写)。
+				// console.log(event.target);	//触发事件的dom节点
+				// console.log(event.target.dataset);	//dom节点身上的data-*自定义属性
+				const { categoryname, category1id, category2id, category3id } =
+					event.target.dataset;
+				if (categoryname) {
+					//存在categoryname属性则一定是a标签，就可以跳转
+
+					// 准备路由跳转的参数对象
+					const location = { name: 'search' };
+
+					// 整合传递的query参数
+					const query = { categoryName: categoryname };
+
+					//区分一级分类、二级分类、三级分类
+					if (category1id) {
+						query.category1Id = category1id;
+					} else if (category2id) {
+						query.category2Id = category2id;
+					} else {
+						query.category3Id = category3id;
+					}
+					location.query = query;
+
+					//整合params参数，判断路由中如果有params参数，就需要在传递的时候带上
+					if (this.$route.params) location.params = this.$route.params;
+
+					this.$router.push(location);
+				}
+			},
+			//鼠标移入，商品分类显示
+			showSort() {
+				this.isShow = true;
+			},
+			//鼠标离开，商品分类隐藏，Home组件除外，因为它需要一直显示
+			hideSort() {
+				if (this.$route.path !== '/home') this.isShow = false;
+			},
+		},
+	};
 </script>
 
 <style lang="less" scoped>
-.type-nav {
-	border-bottom: 2px solid #e1251b;
+	.type-nav {
+		border-bottom: 2px solid #e1251b;
 
-	.container {
-		width: 1200px;
-		margin: 0 auto;
-		display: flex;
-		position: relative;
+		.container {
+			width: 1200px;
+			margin: 0 auto;
+			display: flex;
+			position: relative;
 
-		.all {
-			width: 210px;
-			height: 45px;
-			background-color: #e1251b;
-			line-height: 45px;
-			text-align: center;
-			color: #fff;
-			font-size: 14px;
-			font-weight: bold;
-		}
-
-		.nav {
-			a {
+			.all {
+				width: 210px;
 				height: 45px;
-				margin: 0 22px;
+				background-color: #e1251b;
 				line-height: 45px;
-				font-size: 16px;
-				color: #333;
+				text-align: center;
+				color: #fff;
+				font-size: 14px;
+				font-weight: bold;
 			}
-		}
 
-		.sort {
-			position: absolute;
-			left: 0;
-			top: 45px;
-			width: 210px;
-			height: 461px;
-			position: absolute;
-			background: #fafafa;
-			z-index: 999;
+			.nav {
+				a {
+					height: 45px;
+					margin: 0 22px;
+					line-height: 45px;
+					font-size: 16px;
+					color: #333;
+				}
+			}
 
-			.all-sort-list2 {
-				.item {
-					h3 {
-						line-height: 30px;
-						font-size: 14px;
-						font-weight: 400;
-						overflow: hidden;
-						padding: 0 20px;
-						margin: 0;
+			.sort {
+				position: absolute;
+				left: 0;
+				top: 45px;
+				width: 210px;
+				height: 461px;
+				position: absolute;
+				background: #fafafa;
+				z-index: 999;
 
-						a {
-							color: #333;
+				.all-sort-list2 {
+					.item {
+						h3 {
+							line-height: 28px;
+							font-size: 14px;
+							font-weight: 400;
+							overflow: hidden;
+							padding: 0 20px;
+							margin: 0;
+
+							a {
+								color: #333;
+							}
 						}
-					}
 
-					.item-list {
-						display: none;
-						position: absolute;
-						width: 734px;
-						min-height: 460px;
-						_height: 200px;
-						background: #f7f7f7;
-						left: 210px;
-						border: 1px solid #ddd;
-						top: 0;
-						z-index: 9999 !important;
+						.item-list {
+							display: none;
+							position: absolute;
+							width: 734px;
+							min-height: 460px;
+							_height: 200px;
+							background: #f7f7f7;
+							left: 210px;
+							border: 1px solid #ddd;
+							top: 0;
+							z-index: 9999 !important;
 
-						.subitem {
-							float: left;
-							width: 650px;
-							padding: 0 4px 0 8px;
+							.subitem {
+								float: left;
+								width: 650px;
+								padding: 0 4px 0 8px;
 
-							dl {
-								border-top: 1px solid #eee;
-								padding: 6px 0;
-								overflow: hidden;
-								zoom: 1;
-
-								&.fore {
-									border-top: 0;
-								}
-
-								dt {
-									float: left;
-									width: 54px;
-									line-height: 22px;
-									text-align: right;
-									padding: 3px 6px 0 0;
-									font-weight: 700;
-								}
-
-								dd {
-									float: left;
-									width: 415px;
-									padding: 3px 0 0;
+								dl {
+									border-top: 1px solid #eee;
+									padding: 3px 0;
 									overflow: hidden;
+									zoom: 1;
 
-									em {
+									&.fore {
+										border-top: 0;
+									}
+
+									dt {
 										float: left;
-										height: 14px;
-										line-height: 14px;
-										padding: 0 8px;
-										margin-top: 5px;
-										border-left: 1px solid #ccc;
+										width: 54px;
+										line-height: 22px;
+										text-align: right;
+										padding: 3px 6px 0 0;
+										font-weight: 700;
+									}
+
+									dd {
+										float: left;
+										width: 415px;
+										padding: 3px 0 0;
+										overflow: hidden;
+
+										em {
+											float: left;
+											height: 14px;
+											line-height: 14px;
+											padding: 0 8px;
+											margin-top: 5px;
+											border-left: 1px solid #ccc;
+										}
 									}
 								}
 							}
 						}
+
+						&:hover {
+							.item-list {
+								display: block;
+							}
+						}
 					}
 
-					&:hover {
-						.item-list {
-							display: block;
-						}
+					.item:hover {
+						background-color: skyblue;
 					}
 				}
 			}
+
+			/* vue封装的动画默认样式名称开头是v，可以改名字
+    		在transition标签加上name="xxx"，name开头就必须是xxx */
+			/*进入的起点,离开的终点  */
+			.sort-enter,
+			.sort-leave-to {
+				opacity: 0;
+			}
+
+			/*进入过程中, 离开过程中 */
+			.sort-enter-active,
+			.sort-leave-active {
+				transition: 0.5 linear;
+			}
+
+			/* 进入的终点，离开的起点 */
+			.sort-enter-to,
+			.sort-leave {
+				opacity: 1;
+			}
 		}
 	}
-}
 </style>
