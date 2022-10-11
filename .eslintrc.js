b1499de916142e8955250fdac41594439ae14df5
LLanQing <44639217+LLanQing@users.noreply.github.com>
2022-10-11 20:21:14 +0800

    完善项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,5 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'vue/multi-word-component-names': 'off', // 不校验组件名
-		'vue/no-multiple-template-root': 0, // 不需要使用根元素包裹template的内容
 	},
 };
